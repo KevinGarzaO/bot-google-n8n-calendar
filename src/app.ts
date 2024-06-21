107339d08e50d28b1949bffdc069798fedaaf0ec
Kevin Garza <kevin_4569@msn.com>
2024-06-21 11:51:34 -0600

    se quito 2 endpoints para validar
    se quito 2 endpoints para validar

--- src/app.ts
+++ src/app.ts
@@ -24,28 +24,6 @@ const main = async () => {
         })
     )
 
-    provider.server.post(
-        '/v1/register',
-        handleCtx(async (bot, req, res) => {
-            const { number, name } = req.body
-            await bot.dispatch('REGISTER_FLOW', { from: number, name })
-            return res.end('trigger')
-        })
-    )
-
-
-    provider.server.post(
-        '/v1/blacklist',
-        handleCtx(async (bot, req, res) => {
-            const { number, intent } = req.body
-            if (intent === 'remove') bot.blacklist.remove(number)
-            if (intent === 'add') bot.blacklist.add(number)
-
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            return res.end(JSON.stringify({ status: 'ok', number, intent }))
-        })
-    )
-
     httpServer(+PORT)
 }
 main()
